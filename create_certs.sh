0a8411934d8b66db0b94f36c0e6556124b4c298f
Carles San Agustin <csanagustin@worldsensing.com>
2017-11-27 17:24:46 +0100

    ssl enabled

---
+++ create_certs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+GIST="https://gist.githubusercontent.com/carlessanagustin/dc8e6a47be27faaa3dbf99caa2a89458/raw/a524a6f7a082e5e0da7b368ff8f01a6c8bfb99d1/create_keys.sh"
+wget $GIST
+/bin/bash create_keys.sh
