cd7b1134433f40e1694b9c342d26f09a50d43e09
Carles San Agustin <csanagustin@worldsensing.com>
2017-11-27 18:23:22 +0100

    telegraf instance installation

--- telegraf/install_telegraf_Ubuntu.sh
+++ telegraf/install_telegraf_Ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ## run as root
 
-APT_PACKAGES="curl apt-transport-https"
+export APT_PACKAGES="curl apt-transport-https"
 
 apt-get update
 apt-get -y install $APT_PACKAGES
@@ -12,7 +12,9 @@ echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stabl
     tee /etc/apt/sources.list.d/influxdb.list
 apt-get update && apt-get -y install telegraf
 
-# systemctl start telegraf
+# usermod -aG docker telegraf
+# systemctl restart telegraf
 
+# reduce docker images size
 apt-get -y remove $APT_PACKAGES
 rm -rf /var/lib/apt/lists/*
