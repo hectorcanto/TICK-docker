d2ee7a8ebe30d4daaa77680c852936d916c988d4
Carles San Agustin <csanagustin@worldsensing.com>
2018-02-26 15:57:36 +0100

    telegraf version 1.5.2-1 + telegraf.conf split in multiple + docker-compose ready for telegraf.d + updated gitignore

--- build/telegraf/install_telegraf_Ubuntu.sh
+++ build/telegraf/install_telegraf_Ubuntu.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 ## run as root
 
+export VERSION=1.5.2-1
 export APT_PACKAGES="curl apt-transport-https"
 
 apt-get update
@@ -10,7 +11,7 @@ curl -sL https://repos.influxdata.com/influxdb.key | apt-key add -
 source /etc/lsb-release
 echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | \
     tee /etc/apt/sources.list.d/influxdb.list
-apt-get update && apt-get -y install telegraf
+apt-get update && apt-get -y install telegraf=$VERSION
 
 # usermod -aG docker telegraf
 # systemctl restart telegraf
